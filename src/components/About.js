68d4dca9f653c5ae51402c5825fe2641e4286e5b
Alroy Fernandes <alroyfernandes07518@gmail.com>
2021-11-03 18:49:11 +0530

    done with about us section

---
+++ src/components/About.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { FaChevronDown } from "react-icons/fa";
+import ScrollIntoView from "scroll-into-view";
+const About = () => {
+  return (
+    <div className="max-w-4xl mt-20 mx-auto">
+      <p className="text-2xl text-black md:text-4xl font-bold text-center">
+        Hey 👋
+      </p>
+      <p className="text-base text-gray-600 sm:text-xl  text-center leading-relaxed mt-4">
+        I'm a passionate web developer and a competitve coder.I also love
+        contributing to open source
+      </p>
+
+      <ScrollIntoView selector="#tech">
+        <div className="mx-auto p-20">
+          <FaChevronDown className="animate-bouner mx-auto text-3xl text-blue-500" />
+        </div>
+      </ScrollIntoView>
+    </div>
+  );
+};
+
+export default About;
