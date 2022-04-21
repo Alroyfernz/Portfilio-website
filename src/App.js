aea3bf9308185d28f7bfee23bdc02000cf001cb2
Alroy Fernandes <alroyfernandes07518@gmail.com>
2022-04-21 23:19:18 +0530

    glassmorphism added for card

--- src/App.js
+++ src/App.js
@@ -6,12 +6,29 @@ import Footer from "./components/Footer";
 import Self from "./components/Self";
 import Skills from "./components/Skills";
 import AOS from "aos";
+
 import "aos/dist/aos.css"; // You can also use <link> for styles
 // ..
 AOS.init();
 function App() {
+  const particlesInit = (main) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
   return (
-    <div className="min-h-screen py-10 px-3 sm:px-5 bg-gray-100" id="home">
+    <div
+      className="min-h-screen py-10 px-3 sm:px-5"
+      style={{
+        backgroundImage:
+          "url(https://tailwindcss.com/_next/static/media/hero-dark@90.a7a063e8f9d179fbd72b0b735c5797b7.jpg)",
+      }}
+      id="home"
+    >
       {/* <Navbar /> */}
       <div data-aos="fade-down" data-aos-duration="1000">
         <Card />
